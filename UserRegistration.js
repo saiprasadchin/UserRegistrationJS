8bf7430ccad8e49a31162a23781086ec7699a0d8
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-02 11:08:03 -0700

    Added : Regex to check if the entered name is valid or not

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,1 +1,11 @@
 console.log("Welcome to UserRegistration");
+const prompt = require('prompt-sync')();
+
+// RegEx Formats
+let nameFormat = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
+
+const name = prompt("Enter Firs Name: ");
+if(nameFormat.test(name))
+    console.log(`Hello ${name}`);
+else
+    console.log("InValid Name -> Start with capital and minimum 3 later");
