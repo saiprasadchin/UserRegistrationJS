87d4a81c292de0901c43e4b12879efeeb175d2f7
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-02 11:47:43 -0700

    Added : Regex to check email is valid or not

--- UserRegistration.js
+++ UserRegistration.js
@@ -2,30 +2,37 @@ console.log("Welcome to UserRegistration");
 
 const prompt = require('prompt-sync')();
 
+let namePattern = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
+let emailPattern = new RegExp("^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$");
 
-let nameFormat = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
-
-function checkFormat(name){
-    return nameFormat.test(name);
+function checkFormat(input, inputFormat){
+    return inputFormat.test(input);
 }
 
 function getFirstName(){
     let firstName = prompt("Enter First Name: ");
-    if(!checkFormat(firstName)){
+    if(!checkFormat(firstName, namePattern)){
         console.log("Enter Valid First Name");
         getFirstName();
     }
-    return;
 }
 
 function getLastName(){
     let lastName = prompt("Enter Last Name: ");
-    if(!checkFormat(lastName)){
+    if(!checkFormat(lastName, namePattern)){
         console.log("Enter Valid Last Name");
         getLastName();
     }
-    return
+}
+
+function getEmail(){
+    let email = prompt("Enter Email: ");
+    if(!checkFormat(email, emailPattern)){
+        console.log("Enter Valid Email");
+        getEmail();
+    }
 }
 
 getFirstName();
 getLastName();
+getEmail();
