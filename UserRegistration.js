13552195250d62da0d03930c60d80f5018a84fbf
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-02 12:20:34 -0700

    Added : Regex to check passward with minimun eight character

--- UserRegistration.js
+++ UserRegistration.js
@@ -2,9 +2,10 @@ console.log("Welcome to UserRegistration");
 
 const prompt = require('prompt-sync')();
 
-let namePattern = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
-let emailPattern = new RegExp("^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$");
-let phonePattern = new RegExp("^[0-9][0-9] [6-9][0-9]{9}$");
+const namePattern = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
+const emailPattern = new RegExp("^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$");
+const phonePattern = new RegExp("^[0-9][0-9] [6-9][0-9]{9}$");
+const passwordPattern = new RegExp("^.{8,}$");
 
 function checkFormat(input, inputFormat){
     return inputFormat.test(input);
@@ -42,7 +43,16 @@ function readPhoneNumber(){
     }
 }
 
+function readPassword(){
+    let password = prompt("Enter Password: ");
+    if(!checkFormat(password, passwordPattern)){
+        console.log("Enter Valid Password");
+        readPassword();
+    }
+}
+
 getFirstName();
 getLastName();
 getEmail();
 readPhoneNumber();
+readPassword();
