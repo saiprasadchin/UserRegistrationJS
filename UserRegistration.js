b7bf73e22e00c3054295518cf0f68be859908cce
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-08-02 11:31:06 -0700

    Added : Regex to check if the entered last name is valid or not

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,11 +1,31 @@
 console.log("Welcome to UserRegistration");
+
 const prompt = require('prompt-sync')();
 
-// RegEx Formats
+
 let nameFormat = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
 
-const name = prompt("Enter Firs Name: ");
-if(nameFormat.test(name))
-    console.log(`Hello ${name}`);
-else
-    console.log("InValid Name -> Start with capital and minimum 3 later");
+function checkFormat(name){
+    return nameFormat.test(name);
+}
+
+function getFirstName(){
+    let firstName = prompt("Enter First Name: ");
+    if(!checkFormat(firstName)){
+        console.log("Enter Valid First Name");
+        getFirstName();
+    }
+    return;
+}
+
+function getLastName(){
+    let lastName = prompt("Enter Last Name: ");
+    if(!checkFormat(lastName)){
+        console.log("Enter Valid Last Name");
+        getLastName();
+    }
+    return
+}
+
+getFirstName();
+getLastName();
